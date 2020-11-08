595a18a4f001e535188e48399146c2449c9e7e47
Lucas Matos <lucaspmatos@outlook.com>
2020-11-08 18:53:25 -0300

    Creating snake moves

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box,
-}
+  x: 8 * box,
+  y: 8 * box,
+};
+
+let direction = "right";
 
 function createBackground() {
   context.fillStyle = "lightgreen";
@@ -13,11 +15,32 @@ function createBackground() {
 }
 
 function createSnake() {
-    for(i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
-    }
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+  createBackground();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
